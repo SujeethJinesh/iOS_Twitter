4d53907cb5a190563a3cd11a47cbd7ec46c14317
SujeethJinesh <sujeethjinesh@gmail.com>
2022-12-24 20:41:10 -0800

    Initial Commit

---
+++ TwitterSwiftUIYoutubeApp/TwitterSwiftUIYoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIYoutubeAppApp.swift
+//  TwitterSwiftUIYoutubeApp
+//
+//  Created by Sujeeth Jinesh on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIYoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
